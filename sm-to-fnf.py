ee7b80498083786f6f6f5ea292c6eb0bff6bfa41
tzheng22 <t.ho.zheng@gmail.com>
2022-10-26 12:56:14 -0400

    rewritten converter

--- sm-to-fnf.py
+++ sm-to-fnf.py
@@ -18,95 +18,104 @@ for i in bpmsTimestamps:
     timestamps.append(temp[0])
     bpm.append(temp[1])
 
-m = 0
-
-currentBPM = float(bpm[m])
+currentBPMIndex = 0
+currentBPM = float(bpm[currentBPMIndex])
+millisecondsPerBeat = 60000 / currentBPM
 
 notes = []
 bpmofnotes = []
 
 currBeatInMilliSeconds = 0
-
 currBeat = 0
 
-l = 0
+measure = 0
 hold = [0, 0, 0, 0, 0, 0, 0, 0]
 holdtimestamp = [0, 0, 0, 0, 0, 0, 0, 0]
 counthold = [False, False, False, False, False, False, False, False]
-firstnote = []
-while l < len(together):
-    h = 0
-    sectionNotes = []
-    snap = 192 / int((len(together[l]) / 8))
-    while h < len(together[l]) / 8:
-        x = 0
-        if m < len(timestamps) - 1 and float(timestamps[m + 1]) == round(currBeat, 3):
-            m += 1
-            currentBPM = float(bpm[m])
+while measure < len(together):
+    line = 0
+    if currentBPMIndex < len(timestamps) - 1 and float(timestamps[currentBPMIndex + 1]) == round(currBeat, 3):
+        currentBPMIndex += 1
+        currentBPM = float(bpm[currentBPMIndex])
         millisecondsPerBeat = 60000 / currentBPM
-        while x < 8:
-            if counthold[x]:
-                hold[x] += millisecondsPerBeat / 48
-            if together[l][8 * h + x:8 * h + x + 1] == "1":
-                sectionNotes.append([currBeatInMilliSeconds, x, 0, 0])
-            elif together[l][8 * h + x:8 * h + x + 1] == "M":
-                sectionNotes.append([currBeatInMilliSeconds, x, 0, 3])
-            elif together[l][8 * h + x:8 * h + x + 1] == "2":
-                counthold[x] = True
-                holdtimestamp[x] = currBeatInMilliSeconds
-            elif together[l][8 * h + x:8 * h + x + 1] == "3":
-                counthold[x] = False
-                o = 0
-                q = 0
-                p = 0
-                if len(notes) > 0:
-                    if len(notes[o]) > 0:
-                        while o < len(notes):
-                            p = 0
-                            while p < len(notes[o]) and holdtimestamp[x] > notes[o][p][0]:
-                                p += 1
-                            if p < len(notes[o]):
-                                q = p
-                                break
-                            o += 1
-                        if o == len(notes):
-                            sectionNotes.append([holdtimestamp[x], x, hold[x], 0])
-                        else:
-                            notes[o].insert(q, [holdtimestamp[x], x, hold[x], 0])
-                    else:
-                        sectionNotes.append([holdtimestamp[x], x, hold[x], 0])
+    while line < len(together[measure]) / 8:
+        lane = 0
+        while lane < 8:
+            if counthold[lane]:
+                hold[lane] += millisecondsPerBeat / 48
+            if together[measure][8 * line + lane:8 * line + lane + 1] == "1":
+                notes.append([currBeatInMilliSeconds, lane, 0, 0])
+            if together[measure][8 * line + lane:8 * line + lane + 1] == "M":
+                notes.append([currBeatInMilliSeconds, lane, 0, 3])
+            if together[measure][8 * line + lane:8 * line + lane + 1] == "2":
+                counthold[lane] = True
+                holdtimestamp[lane] = currBeatInMilliSeconds
+            if together[measure][8 * line + lane:8 * line + lane + 1] == "3":
+                counthold[lane] = False
+                noteIndex = 0
+                while noteIndex < len(notes) and round(holdtimestamp[lane], 6) > round(notes[noteIndex][0], 6):
+                    noteIndex += 1
+                if noteIndex == len(notes):
+                    notes.append([holdtimestamp[lane], lane, hold[lane], 0])
                 else:
-                    sectionNotes.append([holdtimestamp[x], x, hold[x], 0])
-                hold[x] = 0
-                holdtimestamp[x] = 0
-            x += 1
-        currBeatInMilliSeconds += millisecondsPerBeat / (48 / snap)
-        currBeat += 1 / (48 / snap)
-        h += 1
-    changeBPM = m > 0 and currentBPM != bpm[m - 1]
+                    notes.insert(noteIndex, [holdtimestamp[lane], lane, hold[lane], 0])
+                hold[lane] = 0
+                holdtimestamp[lane] = 0
+            lane += 1
+        currBeatInMilliSeconds += millisecondsPerBeat / 48
+        currBeat += 1 / 48
+        line += 1
+    changeBPM = currentBPMIndex > 0 and currentBPM != bpm[currentBPMIndex - 1]
     if changeBPM:
         changeBPM = "true"
     else:
         changeBPM = "false"
     bpmofnotes.append([currentBPM, changeBPM])
-    notes.append(sectionNotes)
-    l += 1
+    measure += 1
 
-note = []
+sectionNotesCollection = []
+for measure in together:
+    sectionNotesCollection.append([])
+currentBPMIndex = 0
+currentBPM = float(bpm[currentBPMIndex])
+millisecondsPerBeat = 60000 / currentBPM
+totalLengthOfSong = currBeatInMilliSeconds
+currBeatInMilliSeconds = 0
+currBeat = 0
+measure = 0
+noteIndex = 0
+while measure < len(sectionNotesCollection):
+    if currentBPMIndex < len(timestamps) - 1 and float(timestamps[currentBPMIndex + 1]) == currBeat:
+        currentBPMIndex += 1
+        currentBPM = float(bpm[currentBPMIndex])
+        millisecondsPerBeat = 60000 / currentBPM
+    measureLength = millisecondsPerBeat * 4
+    while noteIndex < len(notes) and round(notes[noteIndex][0], 6) < round(measureLength + currBeatInMilliSeconds, 6):
+        sectionNotesCollection[measure].append(notes[noteIndex])
+        noteIndex += 1
+    measure += 1
+    currBeat += 4
+    currBeatInMilliSeconds += measureLength
+
 u = 0
-while u < len(notes):
-    if u < len(notes) - 1:
-        note.append("{\n" + f"\"sectionNotes\":{str(notes[u])},\n\"lengthInSteps\": 16,\n\"bpm\": {bpmofnotes[u][0]},\n\"changeBPM\": {bpmofnotes[u][1]},\n\"mustHitSection\": false" + "\n},\n")
-    else:
-        note.append("{\n" + f"\"sectionNotes\":{str(notes[u])},\n\"lengthInSteps\": 16,\n\"bpm\": {bpmofnotes[u][0]},\n\"changeBPM\": {bpmofnotes[u][1]},\n\"mustHitSection\": false" + "\n}\n")
+jsondump = []
+while u < len(sectionNotesCollection):
+    if u < len(sectionNotesCollection) - 1:
+        jsondump.append(
+            "{\n" + f"\"sectionNotes\":{str(sectionNotesCollection[u])},\n\"lengthInSteps\": 16,\n\"bpm\": {bpmofnotes[u][0]},\n\"changeBPM\": {bpmofnotes[u][1]},\n\"mustHitSection\": false" + "\n},\n")
+    else: # bc stupid ass json breaks if theres an extra comma
+        jsondump.append(
+            "{\n" + f"\"sectionNotes\":{str(sectionNotesCollection[u])},\n\"lengthInSteps\": 16,\n\"bpm\": {bpmofnotes[u][0]},\n\"changeBPM\": {bpmofnotes[u][1]},\n\"mustHitSection\": false" + "\n}\n")
     u += 1
 yourMom = input("song name? ").replace(" ", "-")
 difficulty = input("difficulty? (blank if normal) ").replace(" ", "-")
 if difficulty:
     difficulty = "-" + difficulty
 output = open(f"{yourMom + difficulty}.json", "w")
-output.write("{\n\"song\": {\n\"sectionLengths\": [],\n\"player1\": \"bf\",\n\"events\": [],\n\"gfVersion\": \"gf\",\n\"notes\": [\n")
-for i in note:
+output.write(
+    "{\n\"song\": {\n\"sectionLengths\": [],\n\"player1\": \"bf\",\n\"events\": [],\n\"gfVersion\": \"gf\",\n\"notes\": [\n")
+for i in jsondump:
     output.write(i)
-output.write("],\n\"player2\": \"dad\",\n\"player3\": null,\n\"song\": \"" + yourMom + "\",\n\"validScore\": true,\n\"stage\": \"stage\",\n\"sections\": 0,\n\"needsVoices\": true,\n\"speed\": 3.5,\n" + f"\"bpm\": {bpm[0]}\n" + "}\n}")
+output.write(
+    "],\n\"player2\": \"dad\",\n\"player3\": null,\n\"song\": \"" + yourMom + "\",\n\"validScore\": true,\n\"stage\": \"stage\",\n\"sections\": 0,\n\"needsVoices\": true,\n\"speed\": 3.5,\n" + f"\"bpm\": {bpm[0]}\n" + "}\n}")
 output.close()
